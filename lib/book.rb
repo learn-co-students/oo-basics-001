58e615add4c22e9ff08629bd94feedb2a1b18f6e
Chris Aporta <christopher.aporta@gmail.com>
2015-10-07 00:04:50 -0400

    complete lab + additional content

---
+++ lib/book.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Book
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    unless BRANDS.include?(brand)
+      BRANDS << brand
+    end
+  end
+
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
